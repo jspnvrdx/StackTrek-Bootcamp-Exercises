f83d2ea6fb9cbee4beacb98260c1b6a390fd78e3
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-22 11:44:17 +0800

    Update Chapter4a_Prob04.py

--- Chapter 4/4a/Chapter4a_Prob04.py
+++ Chapter 4/4a/Chapter4a_Prob04.py
@@ -5,21 +5,21 @@ os.system('cls||clear')
 
 magnitude = float(input())
 
-if magnitude <= 2.0:
+if magnitude < 2.0:
     print("Micro")
-elif magnitude >= 2.0 and magnitude <= 3.0:
+elif magnitude >= 2.0 and magnitude < 3.0:
     print("Very Minor")
-elif magnitude >= 3.0 and magnitude <= 4.0:
+elif magnitude >= 3.0 and magnitude < 4.0:
     print("Minor")
-elif magnitude >= 4.0 and magnitude <= 5.0:
+elif magnitude >= 4.0 and magnitude < 5.0:
     print("Light")
-elif magnitude >= 5.0 and magnitude <= 6.0:
+elif magnitude >= 5.0 and magnitude < 6.0:
     print("Moderate")
-elif magnitude >= 6.0 and magnitude <= 7.0:
+elif magnitude >= 6.0 and magnitude < 7.0:
     print("Strong")
-elif magnitude >= 7.0 and magnitude <= 8.0:
+elif magnitude >= 7.0 and magnitude < 8.0:
     print("Major")
-elif magnitude >= 8.0 and magnitude <= 10.0:
+elif magnitude >= 8.0 and magnitude < 10.0:
     print("Great")
 else:
     print("Meteoric")
