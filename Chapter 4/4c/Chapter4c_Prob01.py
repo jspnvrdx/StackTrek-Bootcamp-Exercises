6174419719d09ceb5dfa49b29e842963b8a4bcc6
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-22 20:11:18 +0800

    Create Chapter4c_Prob01.py

---
+++ Chapter 4/4c/Chapter4c_Prob01.py
@@ -0,0 +1,25 @@
+import os
+os.system('cls||clear')
+
+#----CODE STARTS HERE------
+
+from math import sqrt
+a = float(input())
+b = float(input())
+c = float(input())
+
+if a == 0 and b == 0:
+    print("There is not even an unknown in this equation!")
+if (b**2) - (4 * a * c) < 0:
+    print("There are no solutions")
+
+squareroot = (b**2) - (4 * a * c)
+
+if (b**2) - (4 * a * c) > 0:
+    solution1 = (-b + sqrt(squareroot)) / (2 * a)
+    solution2 = (-b - sqrt(squareroot)) / (2 * a)
+    print(f"There are two solutions , namely {solution1} and {solution2}")
+if (b**2) - (4 * a * c) == 0:
+    solution1 = (-b + sqrt(squareroot)) / (2 * a)
+    print(f"There is one solution , namely {solution1}")
+# there should be an error here
