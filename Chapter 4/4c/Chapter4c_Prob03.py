eb148f16192b45b357e1ac144035688b301010a4
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-23 11:31:11 +0800

    Create Chapter4c_Prob03.py

---
+++ Chapter 4/4c/Chapter4c_Prob03.py
@@ -0,0 +1,22 @@
+import os
+os.system('cls||clear')
+
+#----CODE STARTS HERE------
+
+grade = float(input())
+
+if grade >= 0.0 and grade <= 10.0:
+    if (grade % 1 != 0.5) and (grade % 1 != 0.0):
+        print ("Grades should be rounded to the nearest half point.")
+    if grade <= 10 and grade >= 8.5:
+        print("Grade A")
+    if grade <= 8 and grade >= 7.5:
+        print("Grade B")
+    if grade <= 7 and grade >= 6.5:
+        print("Grade C")
+    if grade <= 6 and grade >= 5.5:
+        print("Grade D")
+    if grade < 5.5 and grade >= 0:
+        print("Grade F")
+else:
+    print("Grades should be between zero and 10.")
