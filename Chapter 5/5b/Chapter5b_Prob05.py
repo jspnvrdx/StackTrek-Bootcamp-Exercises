e9f12f712408c0bc3ce877c0dfb41381e0308500
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-25 22:58:04 +0800

    final code for 5b prob05

--- Chapter 5/5b/Chapter5b_Prob05.py
+++ Chapter 5/5b/Chapter5b_Prob05.py
@@ -2,4 +2,19 @@ import os
 os.system('cls||clear')
 
 #----CODE STARTS HERE------
+age = input()
+age_cost = 0
 
+
+while age != 'end':
+    individual = int(age)
+    if (individual >= 3 and individual <= 12):
+        age_cost += 14
+    elif (individual >= 65):
+        age_cost += 18
+    else:
+        age_cost += 23
+
+    age = input()
+
+print(f"{age_cost:.2f}")
