4e9afdc7059f4e9f3c00fc6e558da2ca5487bd14
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-25 21:32:43 +0800

    final code for 5b prob02

--- Chapter 5/5b/Chapter5b_Prob02.py
+++ Chapter 5/5b/Chapter5b_Prob02.py
@@ -3,3 +3,19 @@ os.system('cls||clear')
 
 #----CODE STARTS HERE------
 
+val = int(input())
+count = 1
+average = 0
+if val == 0:
+    print("No entries")
+else:
+    average = val
+    while val != 0:
+        val = int(input())
+        if val == 0:
+            continue
+        average += val
+        count += 1
+
+average /= count
+print(int(average))
