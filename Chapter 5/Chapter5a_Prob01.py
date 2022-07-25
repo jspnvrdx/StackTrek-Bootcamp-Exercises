7ee0f64241ca8070f6f0a64db00609ce93dbb4f9
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-25 19:58:04 +0800

    finalization for exercise 5a prob 1

--- Chapter 5/Chapter5a_Prob01.py
+++ Chapter 5/Chapter5a_Prob01.py
@@ -3,33 +3,33 @@ os.system('cls||clear')
 
 #----CODE STARTS HERE------
 
-LOWER_A = ord('a')
-UPPER_A = ord('A')
-LOWER_Z = ord('z')
-UPPER_Z = ord('Z')
+message = input()
+shift = int(input())
 
-message = input() 
-shift = int(input()) 
+UPPER_A = ord('A')
+UPPER_Z = ord('Z')
+LOWER_A = ord('a')
+LOWER_Z = ord('z')
 new_message = ''
 
-for i in message:
-    asc = ord(i)
-    new_char = asc + shift
-    if (chr(asc) >='A') and (chr(asc) <= 'Z'):
-        new_char = asc + shift
-        if shift > 0 and new_char > UPPER_Z:
-            excess = new_char - UPPER_Z
-            new_char = UPPER_A + excess - 1
-        elif shift < 0 and new_char < UPPER_A:
-            excess = UPPER_A - shift
-            new_char = UPPER_Z - excess + 1
-        new_message += chr(new_char)
-    elif (chr(asc) >='a') and (chr(asc) <= 'z'):
-        if shift > 0 and new_char > LOWER_Z:
-            excess = new_char - LOWER_Z
-            new_char = LOWER_Z + excess - 1
-        elif shift < 0 and new_char > LOWER_A:
-            excess = LOWER_A - shift
-            new_char = LOWER_A + excess - 1   
-        new_message += chr(new_char)
-
+for char in message:
+    asci = ord(char)
+    if chr(asci) >= 'A' and chr(asci) <= 'Z':
+        asci += shift
+        if asci > UPPER_Z:
+            excess = asci - UPPER_Z
+            asci = UPPER_A + excess - 1
+        elif asci < UPPER_A:
+            excess = UPPER_A - asci
+            asci = UPPER_Z - asci
+    elif chr(asci) >= 'a' and chr(asci) <= 'z':
+        asci += shift
+        if asci > LOWER_Z:
+            excess = asci - LOWER_Z
+            asci = LOWER_A + excess - 1
+        elif asci < LOWER_A:
+            excess = LOWER_A - asci
+            asci = UPPER_Z - asci
+    new_message += chr(asci)
+    
+print(new_message)
