ae490bc21ffd90b4ef1861af49371bfbd42089ec
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-25 21:08:21 +0800

    final code for 5b prob01

---
+++ Chapter 5/5b/Chapter5b_Prob01.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls||clear')
+
+#----CODE STARTS HERE------
+
+start = int(input())
+
+while start >= 0:
+    if start == 0:
+        print("Blast off!")
+    else:
+        print(start)
+    start -= 1;
