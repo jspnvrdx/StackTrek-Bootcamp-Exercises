ec1053e9cfb5e4720506f0d956042f38675d26f7
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-07-26 00:10:12 +0800

    chapter 5b prob04 unfinished

--- Chapter 5/5b/Chapter5b_Prob04.py
+++ Chapter 5/5b/Chapter5b_Prob04.py
@@ -3,3 +3,22 @@ os.system('cls||clear')
 
 #----CODE STARTS HERE------
 
+flag = True
+count = 0;
+first_x = 0
+first_y = 0
+temp_x = 0
+temp_y  = 0
+subtotal = 0
+while flag == True:
+    x = input()
+    if(x == 'end'):
+        flag = False
+        continue
+    else:
+        y = input()
+        if count == 0:
+            first_x = int(x)
+            first_y = int(y)
+        else:
+            subtotal += temp_x * int(y) - temp_y * (x)
