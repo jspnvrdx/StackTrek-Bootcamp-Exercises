ff8de65a65ae0fb25f134902cb40dbfdf49ab031
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-08-08 19:07:02 +0800

    finalization for 7a prob02

--- Chapter 7/7a/Chapter7a_Prob02.py
+++ Chapter 7/7a/Chapter7a_Prob02.py
@@ -3,3 +3,18 @@ os.system('cls||clear')
 
 #----CODE STARTS HERE------
 
+def occurence_of_the():
+    if os.path.exists("notes.txt"):
+        f = open("notes.txt", "r")
+    else: 
+        f = open("notes.txt", "x")
+        f = open("notes.txt", "r")
+    count = 0
+    for l in f:
+        for word in l.split():
+            word = word.upper()
+            if word == "THE":
+                count += 1
+    return count
+
+print(occurence_of_the())
