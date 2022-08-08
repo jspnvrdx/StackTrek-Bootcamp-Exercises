3f71a0dd941e38d979bc31ef8c678c774c25e02b
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-08-08 18:32:16 +0800

    final output for 7a prob 01

---
+++ Chapter 7/7a/Chapter7a_Prob01.py
@@ -0,0 +1,27 @@
+import os
+os.system('cls||clear')
+
+#----CODE STARTS HERE------
+
+def jToI():
+    if os.path.exists("WORDS.TXT"):
+        f = open("WORDS.TXT", "r")
+    else: 
+        f = open("WORDS.TXT", "x")
+        f = open("WORDS.TXT", "r")
+
+
+    filecontent = f.read()
+    new_filecontent = ""
+    for i in filecontent:
+        if i == "J":
+            new_filecontent += "I"
+        elif i == "j":
+            new_filecontent += "i"
+        else:
+            new_filecontent += i
+
+    return new_filecontent
+    
+
+jToI()
