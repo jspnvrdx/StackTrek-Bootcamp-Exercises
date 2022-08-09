2542cc287cd2f6b9e31aa4242a4c7993b27fce96
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-08-09 17:37:17 +0800

    final code for 7a prob 04

--- Chapter 7/7a/Chapter7a_Prob04.py
+++ Chapter 7/7a/Chapter7a_Prob04.py
@@ -1,5 +1,39 @@
 import os
+import pickle
 os.system('cls||clear')
 
 #----CODE STARTS HERE------
+def student75():
+    student = open('student.dat', 'rb')
 
+    if os.path.exists('students above 75.dat'):
+        new_student = open('students above 75.dat', 'wb')
+    else:
+        new_student = open('students above 75.dat', 'xb')
+        new_student = open('students above 75.dat', 'wb')
+
+    student_data = []
+    while True:
+        try:
+            participant = pickle.load(student)
+            if participant[2] > 75:
+                student_data.append(participant)
+        except:
+            break
+    
+    for i in student_data:
+        pickle.dump(i,new_student)
+# ----------- Uncomment below if you want to check the display in basket.dat -----------
+
+    # new_student = open("students above 75.dat", "rb")
+    # while True:
+    #         try:
+    #             participant = pickle.load(new_student)
+    #             print(participant)
+    #         except EOFError:
+    #             break
+    
+    student.close()
+    new_student.close()
+
+student75()
