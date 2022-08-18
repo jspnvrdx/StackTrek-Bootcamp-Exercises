03827534eb320bc8c2f881badb3d23508b8face2
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-08-18 12:32:05 +0800

    Update Chapter9c_Prob02.py

--- Chapter 9/9c/Chapter9c_Prob02.py
+++ Chapter 9/9c/Chapter9c_Prob02.py
@@ -5,23 +5,27 @@ os.system('cls||clear')
 
 class WordWizards:
     def __init__(self, player1, player2, prefix):
-        self._player1 = player1
-        self._player2 = player2
-        self._prefix = prefix
+        self.player1 = player1
+        self.player2 = player2
+        self.prefix = prefix.lower()
 
     def player_points(self, player, opponent):
-        # insert code
-        pass
+        player_words = player.words.difference(opponent.words)
+        return sum([len(word) for word in player_words if word.startswith(self.prefix)])
 
     def winner(self):
-        # insert code
-        pass
-
+        player1_score = self.player_points(self.player1, self.player2)
+        player2_score = self.player_points(self.player2, self.player1)
+        if player1_score > player2_score:
+            return self.player1.name
+        if player2_score > player1_score:
+            return self.player2.name
+        return "draw"
 
 class Player:
     def __init__(self, name):
-        self._name = name
+        self.name = name
+        self.words = set()
 
     def add_word(self, word):
-        # insert code
-        pass
+        self.words.add(word.lower())
