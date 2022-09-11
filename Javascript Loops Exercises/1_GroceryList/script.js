ec639e4f24e180d5ec5dac5740c416ce751b6927
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-12 06:48:48 +0800

    grocery list partial code

---
+++ Javascript Loops Exercises/1_GroceryList/script.js
@@ -0,0 +1,5 @@
+const obj = {Meats:["Fish","Chicken","Pork","Beef"],SoapsAndShampoos:["Head n Shoulders","safeguard","dove","rejoice"],Vegetables:["Carrots","Pechay","Talong","Sitaw"],CannedGoods:["Corned Beef","sardines","spam","beef loaf"]};
+
+function getList(obj) {
+    
+}
