28a56f48e5e22cb38e2f0c0e15c5478a705ad1d4
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-12 17:48:56 +0800

    test: adding template code for the activity

---
+++ Code Foundations/Chapter 12/12a/Chapter12a_Prob01.py
@@ -0,0 +1,21 @@
+class FindPath:
+    def __init__(self, root):
+        # insert code
+        pass
+
+    def insert(self, key):
+        # insert code
+        pass
+    def check_insertion(self, node, element):
+        # insert code
+        pass
+
+    def search(self, root, key):
+        # insert code
+        pass
+
+
+class Node:
+    def __init__(self, data):
+        # insert code
+        pass
