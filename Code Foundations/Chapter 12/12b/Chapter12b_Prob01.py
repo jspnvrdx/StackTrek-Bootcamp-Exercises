e4d735cc421489ac7b91a72a458bc822a5081fb6
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-14 08:49:57 +0800

    test: adding template code for chapter 12b prob1

---
+++ Code Foundations/Chapter 12/12b/Chapter12b_Prob01.py
@@ -0,0 +1,16 @@
+import os
+os.system('cls||clear')
+
+# ----CODE STARTS HERE------
+
+
+class NumberFinder:
+    def __init__(self, size):
+        self.size = size
+
+    def hash_code(self, key):
+        str_key = str(key)
+
+    def add(self, number):
+        # insert code
+        pass
