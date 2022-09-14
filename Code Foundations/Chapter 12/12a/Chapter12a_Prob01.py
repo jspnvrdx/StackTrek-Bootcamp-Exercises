29d980cb32b32830f958c85187b687d5dc4ded91
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-14 08:41:43 +0800

    feat: final code - completing code for chapter 12 KPOP

--- Code Foundations/Chapter 12/12a/Chapter12a_Prob01.py
+++ Code Foundations/Chapter 12/12a/Chapter12a_Prob01.py
@@ -1,21 +1,56 @@
+import os
+os.system('cls||clear')
+
+#----CODE STARTS HERE------
+
 class FindPath:
     def __init__(self, root):
-        # insert code
-        pass
+        self.root = root
 
     def insert(self, key):
-        # insert code
-        pass
+        self.root = self.check_insertion(self.root, key)
+      
     def check_insertion(self, node, element):
-        # insert code
-        pass
+        if node == None:
+            newNode = Node(element)
+            return newNode
+        elif node.data < element:
+            newNode = self.check_insertion(node.right, element)
+            node.right = newNode
+        else:
+            newNode = self.check_insertion(node.left, element)
+            node.left = newNode
+        return node
 
-    def search(self, root, key):
-        # insert code
-        pass
+    def search(self, node, key):
+        if node == None:
+            return []
+        elif node.data < key:
+            traversedList = self.search(node.right, key)
+            if len(traversedList) == 0:
+                return []
+            return [node.data] + traversedList
+        elif node.data > key:
+            traversedList = self.search(node.left, key)
+            if len(traversedList) == 0:
+                return []
+            return [node.data] + traversedList
+        else:
+            return [node.data]
 
 
 class Node:
     def __init__(self, data):
-        # insert code
-        pass
+        self.left = None
+        self.right = None
+        self.data = data
+
+root = Node(3)
+tree = FindPath(root)
+tree.insert(3)
+tree.insert(5)
+tree.insert(4)
+tree.insert(2)
+tree.insert(1)
+
+print(tree.search(root, 4))
