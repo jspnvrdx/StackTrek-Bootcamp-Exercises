31024abf9e5d19273614dc9ee092d178107390fe
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-15 16:22:35 +0800

    test: initial code for 12b prob02

---
+++ Code Foundations/Chapter 12/12b/Chapter12b_Prob02.py
@@ -0,0 +1,28 @@
+import os
+os.system('cls||clear')
+
+# ----CODE STARTS HERE------
+
+
+class Rock:
+    def __init__(self, color, count):
+        self.color = color
+        self.count = count
+
+
+class RockShelf:
+    def __init__(self, size):
+        # insert code
+        pass
+
+    def hash_code(self, key):
+        # insert code
+        pass
+
+    def insert(self, item):
+        # insert code
+        pass
+
+    def search(self, color):
+        # insert code
+        pass
