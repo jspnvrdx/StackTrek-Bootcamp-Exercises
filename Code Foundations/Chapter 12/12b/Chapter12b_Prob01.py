5f1ee722b7de227d756664b612468d575ced76ec
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-15 16:05:44 +0800

    feat: Final code for 12bprob01 - "Not There"

--- Code Foundations/Chapter 12/12b/Chapter12b_Prob01.py
+++ Code Foundations/Chapter 12/12b/Chapter12b_Prob01.py
@@ -7,10 +7,25 @@ os.system('cls||clear')
 class NumberFinder:
     def __init__(self, size):
         self.size = size
+        self.hashTable = [None] * size
 
     def hash_code(self, key):
-        str_key = str(key)
+        squared_key = key ** 2
+        str_key = str(squared_key)
+        key_len = len(str_key)
+        half_len = key_len // 2
+        first_half = str_key[:half_len]
+        second_half = str_key[half_len:]
+        if key_len % 2 != 0: #odd number of digits
+            return int(second_half[0])
+        return int(f'{first_half[-1]}{second_half[0]}') 
 
     def add(self, number):
-        # insert code
-        pass
+        index = self.hash_code(number)
+        if self.hashTable[index] is None and index < self.size:
+            self.hashTable[index] = number
+            return index
+        return self.hashTable[index]
+            
+finder = NumberFinder(30)
+finder.add(5)
