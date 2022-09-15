cccc5d1c7b5b6f1d73fea3a7ac515d052258df9d
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-15 23:44:33 +0800

    feat: near completion of code for checkboxes
    bug: labels isn't showing

--- Javascript Loops Exercises/1_GroceryList/script.js
+++ Javascript Loops Exercises/1_GroceryList/script.js
@@ -1,5 +1,33 @@
-const obj = {Meats:["Fish","Chicken","Pork","Beef"],SoapsAndShampoos:["Head n Shoulders","safeguard","dove","rejoice"],Vegetables:["Carrots","Pechay","Talong","Sitaw"],CannedGoods:["Corned Beef","sardines","spam","beef loaf"]};
+const meats = ["Fish","Chicken","Pork","Beef"];
+const soapsAndShampoos = ["Head n Shoulders","safeguard","dove","rejoice"];
+const vegetables = ["Carrots","Pechay","Talong","Sitaw"];
+const cannedGoods = ["Corned Beef","sardines","spam","beef loaf"];
 
-function getList(obj) {
-    
+const meatId = "meats";
+const soapId = "soaps";
+const vegetableId = "vegetables";
+const cannedGoodsId = "canned-goods";
+
+function getList(list, id) {
+    const idtag = document.getElementById(id);
+    for (let x of list){
+        const tag = document.createElement("input");
+        tag.setAttribute("type", "checkbox");
+        tag.setAttribute("name", id.slice(0,-1));
+        tag.setAttribute("id", x.split(" ")[0]);
+        const label = document.createElement("label");
+        label.setAttribute("for", x.split(" ")[0])
+        const text = document.createTextNode(x);
+        label.appendChild(text);
+        tag.appendChild(label);
+        idtag.appendChild(tag)
+        
+        const br = document.createElement("br");
+        idtag.appendChild(br);
+    }
 }
+
+getList(meats, meatId);
+getList(soapsAndShampoos, soapId);
+getList(vegetables, vegetableId);
+getList(cannedGoods, cannedGoodsId);
