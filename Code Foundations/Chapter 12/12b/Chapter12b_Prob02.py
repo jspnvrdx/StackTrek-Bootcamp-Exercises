c288e93759291cf13ae00cd2e75752dd9253a4f9
jspnvrdx <109405424+jspnvrdx@users.noreply.github.com>
2022-09-16 22:07:27 +0800

    feat: final code for 12b prob02 - "Rock You"

--- Code Foundations/Chapter 12/12b/Chapter12b_Prob02.py
+++ Code Foundations/Chapter 12/12b/Chapter12b_Prob02.py
@@ -12,17 +12,50 @@ class Rock:
 
 class RockShelf:
     def __init__(self, size):
-        # insert code
-        pass
+        self.size = size
+        self.hashTable = [None] * size
 
     def hash_code(self, key):
-        # insert code
-        pass
+        code = sum(ord(char) for char in key)
+        return code % len(self.hashTable)
 
     def insert(self, item):
-        # insert code
-        pass
+        hash_index = self.hash_code(item.color.lower())
+        index_data = self.hashTable[hash_index]
+
+        if index_data is None:
+            self.hashTable[hash_index] = item
+            return item.count
+        elif (index_data is not None) and (index_data.color.lower() == item.color.lower()):
+            self.hashTable[hash_index].count += item.count
+        else:
+            while self.hashTable[hash_index] is not None:
+                hash_index += 1
+                if hash_index == len(self.hashTable):
+                    hash_index = 0
+            self.hashTable[hash_index] = item
+        return self.hashTable[hash_index].count
 
     def search(self, color):
-        # insert code
-        pass
+        index = 0
+        for x in self.hashTable:
+            if x is None:
+                continue
+            if x.color.lower() == color.lower():
+                return x.count
+        return 0
+
+rock1 = Rock("green", 5)
+rock2 = Rock("orange", 1)
+rock3 = Rock("BLUE", 12)
+shelf = RockShelf(10)
+shelf.insert(rock1)
+shelf.insert(rock2)
+shelf.insert(rock3)
+print(shelf.search("red"))
+# rock1 = Rock("green", 5)
+# rock2 = Rock("gREeN", 5)
+# shelf = RockShelf(10)
+# print(shelf.insert(rock1))
+# print(shelf.insert(rock2))
+# print(shelf.search("green"))
